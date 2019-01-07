c3b147131f8d9aab1e730dbd091479aeb8c4a0d2
Matheus Barbosa <matheuskleber@live.com>
2019-01-07 16:24:15 -0200

    chore(CORS): adding cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalFilter } from './common/filter/global.filter'
+import * as cors from 'cors'
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
   //app.useGlobalFilters(new GlobalFilter() )
   await app.listen(parseInt(process.env.PORT) || 3001);
 }
